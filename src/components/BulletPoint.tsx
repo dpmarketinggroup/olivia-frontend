a33544358fac8522a75a3eccf559ff91d5124791
OliverSoucc <oliv572v@easv365.dk>
2022-09-16 01:43:23 +0200

    Description

---
+++ src/components/BulletPoint.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from "react";
+
+interface BulletPointProps{
+    icon: JSX.Element; 
+    text: string
+}
+
+const BulletPoint : FunctionComponent<BulletPointProps> = ({
+    icon,
+    text
+}) =>{
+    return(
+        <div className="flex gap-[20px]">
+            {icon}
+            <p className="font-bold text-[16px] leading-[24px] tracking-[0.1px] text-neutral-600">{text}</p>
+        </div>
+    )
+}
+
+export default BulletPoint;
