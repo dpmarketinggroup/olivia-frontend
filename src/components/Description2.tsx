206616e3b16c490b3d9692808621b436260dd885
OliverSoucc <oliv572v@easv365.dk>
2022-09-16 21:00:00 +0200

    Description 2

--- src/components/Description2.tsx
+++ src/components/Description2.tsx
@@ -1,62 +1,69 @@
 import React, { FunctionComponent } from "react";
 import Image from "next/image";
-// import BulletPoint from "./BulletPoint";
+import BulletPoint2 from "./BulletPoint2";
 import Link from "next/link";
 
-interface DescriptionProps {
+interface Description2Props {
   imgPath: string;
   title: string;
-  descrip: string;
   bulletPoints2: [
-    { icon: JSX.Element, title:string, text: string },
-    { icon: JSX.Element, title:string, text: string },
+    { icon: JSX.Element; title: string; text: string },
+    { icon: JSX.Element; title: string; text: string }
   ];
-  buttons: [
-    { path: string; text: string }, 
-    { path: string; text: string }
-];
-  reverseFlex?: boolean;
+  buttons: [{ path: string; text: string }, { path: string; text: string }];
 }
 
-const Description: FunctionComponent<DescriptionProps> = ({
+const Description2: FunctionComponent<Description2Props> = ({
   imgPath,
   title,
-  descrip,
   bulletPoints2,
   buttons,
-  reverseFlex = false,
 }) => {
   return (
-    <div className={`flex gap-[110px] ${reverseFlex && "flex-row-reverse"} mx-[165px] items-center justify-center`}>
-      <div className="h-[400px] w-[555px] relative">
-        <Image objectFit="cover" layout="fill" alt="hero image" src={imgPath} />
-      </div>
-      <div className="flex flex-col gap-[23px]">
-        <h3 className="font-bold text-[40px] leading-[48px] tracking-[-0.5px] text-primary">
-          {title}
-        </h3>
-        <p className="font-normal text-[16px] leading-[24px] tracking-[0.1px] text-neutral-600 w-[445px]">
-          {descrip}
-        </p>
-        {bulletPoints2.map((b, k) => (
-          <BulletPoint key={k} icon={b.icon} text={b.text} />
-        ))}
-        <div className="flex gap-[12px]">
-          {buttons.map((b, k) => (
-            <Link key={k} href={b.path}>
-              <button
-                className={`font-medium text-[16px] leading-[24px] tracking-[0.1px] border h-[36px]
-                  ${k === 0 && "text-green2 bg-white border-[#476761] w-[120px]"} 
-                  ${k === 1 && "text-white bg-[#476761] w-[190px]"}`}
-              >
-                {b.text}
-              </button>
-            </Link>
-          ))}
+    <div className="h-[1000px] mx-[165px]">
+      <h3 className="mb-[95px] font-bold text-[40px] leading-[48px] tracking-[-0.5px] text-center text-primary">
+        {title}
+      </h3>
+      <div className="flex gap-[110px] mb-[95px] justify-center">
+        <div className="h-[400px] w-[555px] relative">
+          <Image
+            objectFit="cover"
+            layout="fill"
+            alt="hero image"
+            src={imgPath}
+          />
         </div>
+        <div>
+          <p className="mb-[45px] font-bold text-[24px] leading-[32px] tracking-[-0.1px]">
+            <span className="text-green2 underline underline-offset-[6px]">Vysoký štandard</span> <br /> pre Váš maximálny komfort
+          </p>
+          <div className="flex gap-[65px]">
+            {bulletPoints2.map((b2, k) => (
+              <BulletPoint2
+                key={k}
+                icon={b2.icon}
+                title={b2.title}
+                text={b2.text}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-[12px] justify-center">
+        {buttons.map((b, k) => (
+          <Link key={k} href={b.path}>
+            <button
+              className={`font-medium text-[16px] leading-[24px] tracking-[0.1px] border h-[50px] w-[190px]
+            ${k === 0 && "text-green2 bg-white border-[#476761]"} 
+            ${k === 1 && "text-white bg-[#476761]"}`}
+            >
+              {b.text}
+            </button>
+          </Link>
+        ))}
       </div>
     </div>
   );
 };
 
-export default Description;
+export default Description2;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import Description2 from "../components/Description2";
 import CloseIcon from "../components/svg/Close";
+import KeyIcon from "../components/svg/Key";
 import RightArrowIcon from "../components/svg/RightArrow";
+import StarIcon from "../components/svg/Star";
 import WholeProcess from "../components/WholeProcess";
 
 const Home: NextPage = () => {
@@ -20,29 +23,53 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className=" mr-[13px]">
-        <CloseIcon />
+          <CloseIcon />
         </div>
       </div>
-      <section className="bg-[url('/img/hero.jpg')] h-[650px] w-full max-w-[1440px] mb-[140px]">
-        <div className="w-[600px] ml-[165px] pt-[185px] mb-[40px]">
-          <h1 className="font-bold  text-[56px] leading-[60px] tracking-[-1px] text-white">
-            Moderný štýl bývania v dynamickej lokalite so skvelými možnosťami
-          </h1>
-        </div>
-        <div className="flex gap-[10px] ml-[165px] ">
-          <Link href="/">
-            <button className="bg-[#89A6A2] w-[210px] h-[50px] text-white font-medium text-[16px] leading-[24px] tracking-[-0.1px]">
-              Nezáväzné stretnutie
-            </button>
-          </Link>
-          <Link href="/">
-            <button className="font-medium text-[16px] leading-[24px] tracking-[-0.1px] w-[210px] h-[50px] border border-white text-white">
-              Ponuka apartmánov
-            </button>
-          </Link>
+      <section className="flex justify-center">
+        <div className="bg-[url('/img/hero.jpg')] h-[650px] w-full max-w-[1440px] mb-[140px]">
+          <div className="w-[600px] ml-[165px] pt-[185px] mb-[40px]">
+            <h1 className="font-bold  text-[56px] leading-[60px] tracking-[-1px] text-white">
+              Moderný štýl bývania v dynamickej lokalite so skvelými možnosťami
+            </h1>
+          </div>
+          <div className="flex gap-[10px] ml-[165px] ">
+            <Link href="/">
+              <button className="bg-[#89A6A2] w-[210px] h-[50px] text-white font-medium text-[16px] leading-[24px] tracking-[-0.1px]">
+                Nezáväzné stretnutie
+              </button>
+            </Link>
+            <Link href="/">
+              <button className="font-medium text-[16px] leading-[24px] tracking-[-0.1px] w-[210px] h-[50px] border border-white text-white">
+                Ponuka apartmánov
+              </button>
+            </Link>
+          </div>
         </div>
       </section>
       <WholeProcess />
+      <Description2
+        imgPath="/img/room2.jpg"
+        title="Vybavenie apartmánov"
+        descrip="Vysoký štandard
+      pre Váš maximálny komfort"
+        bulletPoints2={[
+          {
+            icon: <KeyIcon />,
+            title: "Apartmán na kľúč",
+            text: "Bývanie v Olivia Residence je úplne bezstarostné. Ponúkame komplexne zariadené apratmány vrátane kompletne vybavenej kuchyne so spotrebičmi a sanitou. Vašou jedinou starosťou bude výber nábytku.",
+          },
+          {
+            icon: <StarIcon />,
+            title: "Vysoký štandard vybavenia",
+            text: "Olivia nevyniká len množstvom nadštandardného vybavenia, ale aj jeho prevedením. Materiály použité v apartmánoch zodpovedjú vysokým štandardom kvality.",
+          },
+        ]}
+        buttons={[
+          { path: "/", text: "Vybavenie apartmánov" },
+          { path: "/", text: "Nezáväzné stretnutie" },
+        ]}
+      />
     </>
   );
 };
