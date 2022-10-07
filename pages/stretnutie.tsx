9ccab7a081326bfe4bbc8ccd25cf8eb50d7e34de
OliverSoucc <oliv572v@easv365.dk>
2022-10-07 19:38:41 +0200

    stretnutie page

--- pages/stretnutie.tsx
+++ pages/stretnutie.tsx
@@ -70,77 +70,80 @@ const Stretnutie = () => {
                     </div>
                 </div>
             </div>
-            {/*<div className='flex justify-center bg-[#F5F5F5]'>*/}
-            {/*    <div>*/}
-            {/*        <div className={`flex flex-col xl:flex-row xl:gap-[85px] w-full items-center xl:items-start`}>*/}
-            {/*            <div>*/}
-            {/*                <div className="flex flex-col gap-[20px] mt-[55px] xl:mt-[75px] mx-[165px] mb-[45px]">*/}
-            {/*                    <p className="font-medium text-[14px] leading-5 text-[#999999] xl:w-[455px]">Dohodnite*/}
-            {/*                        si*/}
-            {/*                        nezáväzné stretnutie alebo sa u nás zastavte v čase otváracej doby. Radi Vás*/}
-            {/*                        privítame a zodpovieme Vaše otázky.</p>*/}
-            {/*                    <div className="flex flex-col">*/}
-            {/*                    <span*/}
-            {/*                        className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">PO - PIA</span>*/}
-            {/*                        <span*/}
-            {/*                            className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">10.00 - 18.00</span>*/}
-            {/*                    </div>*/}
-            {/*                    <div className="flex flex-col">*/}
-            {/*                    <span*/}
-            {/*                        className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">SO - NE</span>*/}
-            {/*                        <span*/}
-            {/*                            className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">Zatvorené</span>*/}
-            {/*                    </div>*/}
-            {/*                    <div className="flex flex-col">*/}
-            {/*                    <span*/}
-            {/*                        className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">Adresa</span>*/}
-            {/*                        <span*/}
-            {/*                            className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">Rožňavská 1A, Bratislava</span>*/}
-            {/*                        <div className="flex gap-[12px] items-center">*/}
-            {/*                            <MapPoint2 width="16" height="20" fill="000000"/>*/}
-            {/*                            <p className="font-medium text-[18px] leading-7">Fresh Market | Prízemie</p>*/}
-            {/*                        </div>*/}
-            {/*                    </div>*/}
-            {/*                </div>*/}
-            {/*                <Link href="https://goo.gl/maps/CVbzvKFDCXY9aeaK6">*/}
-            {/*                    <button*/}
-            {/*                        className="hover:bg-primary font-medium text-[16px] leading-6 tracking-[0.1px] text-white py-[12px] px-[45px] bg-[#476761] ml-[165px] mb-[55px] xl:mb-[75px]">Navigovať*/}
-            {/*                    </button>*/}
-            {/*                </Link>*/}
-            {/*            </div>*/}
-            {/*            <div className={`h-[385px] w-[400px] xl:w-[540px] relative xl:top-[75px]`}>*/}
-            {/*                <Image*/}
-            {/*                    objectFit="cover"*/}
-            {/*                    layout="fill"*/}
-            {/*                    alt="hero image"*/}
-            {/*                    src={"/img/smallMap.jpg"}*/}
-            {/*                />*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            {/*<div className='flex justify-center'>*/}
-            {/*    <div className="flex flex-col xl:flex-row gap-[30px] xl:gap-[240px] items-center">*/}
-            {/*        <div*/}
-            {/*            className="h-[250px] xl:h-[385px] w-[340px] xl:w-[540px] max-w-full relative mt-[200px] mb-[25px] xl:my-[115px]">*/}
-            {/*            <Image*/}
-            {/*                objectFit="cover"*/}
-            {/*                layout="fill"*/}
-            {/*                alt="hero image"*/}
-            {/*                src={"/img/map3d.png"}*/}
-            {/*            />*/}
-            {/*        </div>*/}
-            {/*        <div className="flex flex-col gap-[8px]">*/}
-            {/*            <span*/}
-            {/*                className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">Tu sa nachádzame</span>*/}
-            {/*            <span className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">Rožňavská 1A, Bratislava</span>*/}
-            {/*            <div className="flex gap-[12px] items-center mb-[200px] xl:mb-0">*/}
-            {/*                <MapPoint2 width="16" height="20" fill="000000"/>*/}
-            {/*                <p className="font-medium text-[18px] leading-7">Fresh Market | Prízemie</p>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className='xl:flex bg-[#F5F5F5] w-full xl:max-w-[1920px]'>
+                <div className={'xl:mx-auto'}>
+                    <div className={`flex flex-col xl:flex-row  xl:gap-[85px]`}>
+                        <div>
+                            <div className="flex flex-col gap-[20px] mt-[55px] xl:mt-[75px] mx-4 mb-[45px]">
+                                <p className="font-medium text-[14px] leading-5 text-[#999999] xl:w-[455px] text-center xl:text-start">Dohodnite
+                                    si
+                                    nezáväzné stretnutie alebo sa u nás zastavte v čase otváracej doby. Radi Vás
+                                    privítame a zodpovieme Vaše otázky.</p>
+                                <div className="flex flex-col items-center xl:items-start">
+                                <span
+                                    className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">PO - PIA</span>
+                                    <span
+                                        className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">10.00 - 18.00</span>
+                                </div>
+                                <div className="flex flex-col items-center xl:items-start">
+                                <span
+                                    className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">SO - NE</span>
+                                    <span
+                                        className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">Zatvorené</span>
+                                </div>
+                                <div className="flex flex-col items-center xl:items-start">
+                                <span
+                                    className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">Adresa</span>
+                                    <span
+                                        className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">Rožňavská 1A, Bratislava</span>
+                                    <div className="flex gap-[12px] items-center">
+                                        <MapPoint2 width="16" height="20" fill="000000"/>
+                                        <p className="font-medium text-[18px] leading-7">Fresh Market | Prízemie</p>
+                                    </div>
+                                </div>
+                                <Link href="https://goo.gl/maps/CVbzvKFDCXY9aeaK6">
+                                    <button
+                                        className="hover:bg-primary font-medium text-[16px] leading-6 tracking-[0.1px] text-white py-[12px] px-[45px] bg-[#476761] mb-[55px] xl:mb-[75px]">Navigovať
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="flex justify-center xl:my-auto">
+                            <Image
+                                quality={100}
+                                objectFit="cover"
+                                width={540}
+                                height={385}
+                                alt="Mapa"
+                                src={"/img/smallMap.jpg"}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-center mx-4 xl:mx-0'>
+                <div className="flex flex-col xl:flex-row gap-[30px] xl:gap-[240px] items-center">
+                    <div className="flex justify-center mt-[200px] xl:my-[115px]">
+                        <Image
+                            quality={100}
+                            objectFit="cover"
+                            width={540}
+                            height={385}
+                            alt="3d map"
+                            src={"/img/map3d.png"}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-[8px]">
+                        <span
+                            className="font-medium text-[14px] leading-5 text-[#999999] opacity-[96]">Tu sa nachádzame</span>
+                        <span className="font-bold text-[24px] leading-[32px] tracking-[-0.1px] opacity-[96]">Rožňavská 1A, Bratislava</span>
+                        <div className="flex gap-[12px] items-center mb-[200px] xl:mb-0">
+                            <MapPoint2 width="16" height="20" fill="000000"/>
+                            <p className="font-medium text-[18px] leading-7">Fresh Market | Prízemie</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
