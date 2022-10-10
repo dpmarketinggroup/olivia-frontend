272c3a267b8725de3f4052c4b3376684309242b0
OliverSoucc <oliv572v@easv365.dk>
2022-10-10 14:12:14 +0200

    retail

--- pages/retail.tsx
+++ pages/retail.tsx
@@ -15,29 +15,29 @@ const Retail = () => {
                 <Hero imgName="retailHero" icon={<Flat/>} title="Retail"
                       description="Lukratívna lokalita pre život aj biznis"/>
                 <div className="flex justify-center">
-                    <div className="mb-[120px] xl:mb-[165px]">
+                    <div className="mb-[120px] xl:mb-[165px] w-full">
                         <div className="flex flex-col xl:flex-row gap-[30px] xl:gap-[60px]">
                             <div
-                                className="flex flex-col xl:flex-row items-center xl:items-start gap-[50px] w-[400px] xl:w-[700px] bg-[#F5F5F5] py-[90px] px-4 xl:pl-[135px] xl:pr-0">
-                                <div className="w-[190px]">
+                                className="flex flex-col xl:flex-row items-center xl:items-start gap-[50px] xl:max-w-[700px] bg-[#F5F5F5] py-[90px] px-4 xl:px-[135px] mt-[-60px] xl:mt-0 ml-0 xl:ml-auto">
+                                <div className={'flex xl:block flex-col items-center xl:max-w-[190px] '}>
                                     <FlatBold/>
-                                    <h4 className="font-medium text-[16px] leading-6 tracking-[0.1px] mt-[22px] mb-[8px]">Prenájom
+                                    <h4 className="font-medium text-[18px] xl:text-[16px] leading-6 tracking-[0.1px] mt-[22px] mb-[8px]">Prenájom
                                         obchodného priestoru</h4>
-                                    <p className="text-[14px] leading-5 text-[#676766]">Prenajmite si <strong>obchodný
+                                    <p className="text-[16px] xl:text-[14px] leading-5 text-[#676766] text-center xl:text-start">Prenajmite si <strong>obchodný
                                         priestor v dynamickej lokalite </strong>, kde Váš biznis určite nesplynie s
                                         konkurenciou.</p>
                                 </div>
-                                <div className="w-[190px]">
+                                <div className={'flex xl:block flex-col items-center xl:max-w-[190px]'}>
                                     <FloorPlan/>
-                                    <h4 className="font-medium text-[16px] leading-6 tracking-[0.1px]  mt-[22px] mb-[8px]">Pôdorys
+                                    <h4 className="font-medium text-[18px] xl:text-[16px] leading-6 tracking-[0.1px]  mt-[22px] mb-[8px]">Pôdorys
                                         na mieru</h4>
-                                    <p className="text-[14px] leading-5 text-[#676766]">Pre prvých pár záujemcov
+                                    <p className="text-[16px] xl:text-[14px] leading-5 text-[#676766] text-center xl:text-start">Pre prvých pár záujemcov
                                         ponúkame jedinečnú možnosť <strong>návrhu obchodného priestoru na
                                             mieru</strong> podľa Vašich potrieb.</p>
                                 </div>
                             </div>
-                            <div className="ml-4 xl:ml-0">
-                                <p className="font-medium text-[16px] leading-6 tracking-[0.1px] text-[#676766] w-[335px] mb-[23px]">V
+                            <div className="mx-auto xl:ml-0">
+                                <p className="hidden xl:block font-medium text-[16px] leading-6 tracking-[0.1px] text-[#676766] w-[335px] mb-[23px]">V
                                     prípade záujmu o prenájom obchodného priestoru ma neváhajte kontaktovať</p>
                                 <div className="flex gap-[20px] items-center mb-[20px]">
                                     <div className="h-[60px] w-[60px] relative">
@@ -49,7 +49,7 @@ const Retail = () => {
                                         />
                                     </div>
                                     <div>
-                                        <p className="font-bold text-[16px] leading-6 tracking-[0.1px] ">Božena
+                                        <p className="font-bold text-[16px] leading-6 tracking-[0.1px]">Božena
                                             Nemcová</p>
                                         <span className="text-[14px] leading-5">Sales Assistant</span>
                                     </div>
