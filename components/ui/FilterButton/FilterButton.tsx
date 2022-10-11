917babf3e7bd6da3d6095828c7d7d01ff9e3769c
pelonidas <janx803y@easv365.dk>
2022-10-11 17:11:27 +0200

    refactorr

--- components/ui/FilterButton/FilterButton.tsx
+++ components/ui/FilterButton/FilterButton.tsx
@@ -5,8 +5,8 @@ interface FilterButtonProps {
     icon?: JSX.Element;
     onClick?: () => void;
     children?: React.ReactNode,
-    clicked?: boolean,
-    className?: string
+    clicked?: boolean;
+    className?: string;
 }
 
 const FilterButton = ({variant, icon, onClick, children, clicked = false, className}: FilterButtonProps) => {
@@ -18,9 +18,12 @@ const FilterButton = ({variant, icon, onClick, children, clicked = false, classN
                 return 'w-[70px] h-[70px] flex items-center justify-center font-bold'
         }
     }
+
     return (
-        <button onClick={onClick} className={`${getClass()} ${clicked ? 'bg-white text-[#0E3F3B]' : 'border-white text-white'} border-2`}>
-            <div className={`${variant === 'square' && 'max-w-[20px]'} ${className} flex items-center gap-[10px]`}>{variant === 'rectangle' && icon}{children}</div>
+        <button onClick={onClick}
+                className={`${getClass()} ${clicked ? 'bg-white text-[#0E3F3B]' : 'border-white text-white'} border-2`}>
+            <div
+                className={`${variant === 'square' && 'max-w-[20px]'} ${className} flex items-center gap-[10px]`}>{variant === 'rectangle' && icon}{children}</div>
         </button>
     );
 };
