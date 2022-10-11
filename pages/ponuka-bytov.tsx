0097200ae094a1e8f1b3d43308d922a155d2da21
OliverSoucc <oliv572v@easv365.dk>
2022-10-12 00:56:31 +0200

    pp

--- pages/ponuka-bytov.tsx
+++ pages/ponuka-bytov.tsx
@@ -5,8 +5,10 @@ import {Select} from "@mantine/core";
 import Head from "next/head";
 import Link from "next/link";
 import {CarDescription} from "@components/common/Description/Description";
+import React, {useState} from "react";
 
 const PonukaBytov = () => {
+    const [isFloorDropDownCLicked, setIsFloorDropDownCLicked] = useState(false)
     return (
         <>
             <Head>
@@ -21,8 +23,53 @@ const PonukaBytov = () => {
                                 <h1 className="font-bold text-[32px] xl:text-[40px] leading-[40px] xl:leading-[48px] text-center xl:text-left">Ponuka
                                     apartmánov</h1>
                             </div>
-                            <div
-                                className="flex flex-col xl:flex-row flex-col-reverse gap-[25px] xl:gap-0 items-center xl:items-start xl:justify-between">
+                            <div className="flex flex-col xl:flex-row flex-col-reverse gap-[25px] xl:gap-0 items-center xl:justify-between xl:mr-[180px]">
+                                <div className="dropdown px-[30px] py-[15px] bg-[#F5F5F5] rounded-[33px] apartment absolute xl:right-[210px] cursor-pointer"
+                                     onClick={() => setIsFloorDropDownCLicked((prevState) => !prevState)}
+                                >
+                                    <span className="drop-span font-bold text-[18px] leading-7 text-[#476761]">7. podlažie</span>
+                                    <div className={`${isFloorDropDownCLicked ? "dropdown-content" : "hidden"}`}>
+                                        <div
+                                            className=" flex flex-col px-[30px] text-[18px] leading-7 text-[#476761] font-medium">
+                                            <Link href={'/podlazie/4'}>
+                                                <a className="py-[7px]">4. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/5'}>
+                                                <a className="py-[7px]">5. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/6'}>
+                                                <a className="py-[7px]">6. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/7'}>
+                                                <a className="py-[7px]">7. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/8'}>
+                                                <a className="py-[7px]">8. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/9'}>
+                                                <a className="py-[7px]">9. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/10'}>
+                                                <a className="py-[7px]">10. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/11'}>
+                                                <a className="py-[7px]">11. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/12'}>
+                                                <a className="py-[7px]">12. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/13'}>
+                                                <a className="py-[7px]">13. podlažie</a>
+                                            </Link>
+                                            <Link href={'/podlazie/14'}>
+                                                <a className="py-[7px]">14. podlažie</a>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                    <div className="ml-[10px] inline-block">
+                                        <ArrowDownNotFilledIcon/>
+                                    </div>
+                                </div>
                                 <div
                                     className="flex gap-[18px] xl:gap-[15px] items-center py-[11px] xl:py-[23px] px-[16px] xl:px-[25px] bg-[#F5F5F5] rounded-[33px] z-10">
                                     <Link href="/ponuka-bytov">
@@ -36,6 +83,9 @@ const PonukaBytov = () => {
                                     <span
                                         className="font-medium text-[12px] xl:text-[14px] leading-5 text-[#00000033] text-center xl:text-left">Detail apartmánu</span>
                                 </div>
+                                {/*<div className="hidden xl:inline-block relative left-[285px]">*/}
+                                {/*    <Severka/>*/}
+                                {/*</div>*/}
                             </div>
                         </div>
                     </div>
