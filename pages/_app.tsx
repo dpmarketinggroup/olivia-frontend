0fa07352627c9a8f3522a90ac1e1aec8f5831e2f
pelonidas <janx803y@easv365.dk>
2022-10-16 11:36:09 +0200

    favicon added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,20 +8,32 @@ import type {AppProps} from 'next/app'
 import {MantineProvider} from "@mantine/core";
 import {Navbar, Footer} from '@components/common'
 import {useRouter} from "next/router";
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
     const router = useRouter();
     return (
-        <MantineProvider>
-            <Navbar mainPage={router.pathname === '/' && true}/>
-            <Component {...pageProps} />
-            {
-                router.pathname === '/404' ||
-                router.pathname === '/dakujeme' ||
-                router.pathname === '/dakujeme-za-email' ?
-                    <Footer toBottom={true}/> : <Footer/>
-            }
-        </MantineProvider>
+        <>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="msapplication-TileColor" content="#da532c"/>
+                <meta name="theme-color" content="#ffffff"/>
+            </Head>
+            <MantineProvider>
+                <Navbar mainPage={router.pathname === '/' && true}/>
+                <Component {...pageProps} />
+                {
+                    router.pathname === '/404' ||
+                    router.pathname === '/dakujeme' ||
+                    router.pathname === '/dakujeme-za-email' ?
+                        <Footer toBottom={true}/> : <Footer/>
+                }
+            </MantineProvider>
+        </>
     )
 }
 
