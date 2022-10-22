cc089822a27442e47117c0b2a0a5766bcf34423c
pelonidas <janx803y@easv365.dk>
2022-10-22 20:40:49 +0200

    margrin updates

--- pages/dakujeme-za-email.tsx
+++ pages/dakujeme-za-email.tsx
@@ -9,7 +9,7 @@ function DakujemeZaEmail() {
                 <title>Ďakujeme za email | Olivia Residence</title>
             </Head>
             <p className="font-bold text-[56px] leading-[60px] text-center tracking-[-1px] mt-[185px] text-green2 mb-[50px]">Ďakujeme</p>
-            <p className="text-center mx-auto font-bold text-[24px] leading-[32px] tracking-[-0.1px] mb-[50px] w-[300px] xl:w-[500px]">Na Váš email sme odoslali potvrdzovací mail, jeho potvrdením sa prihlásite k odberu noviniek</p>
+            <p className="text-center mx-auto font-bold text-[24px] leading-[32px] tracking-[-0.1px] mb-[50px] w-[300px] xl:w-[500px]">Úspešne ste sa prihlásili k odberu noviniek.</p>
             <div className='flex gap-[10px] items-center justify-center mb-[185px]'>
                 <LeftArrow stroke="#476761"/>
                 <Link href='/'>
