a7ad22e431683e57a48bc24599ed40c3fdedbde2
pelonidas <janx803y@easv365.dk>
2022-10-22 18:29:11 +0200

    margrin updates

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -64,122 +64,132 @@ const Navbar: FunctionComponent<NavbarProps> = ({mainPage = false}) => {
             {/*        </div>*/}
             {/*    )*/}
             {/*}*/}
-        {/*    ${(mainPage && !cookieConsentIsTrue) && "mt-[40px]"} */}
-        <nav className={`px-[1rem] bg-white shadow-[0_0_10px_rgba(0,0,0,0.15)] xl:h-[60px] w-full fixed top-0 z-50`}>
-            <div className={`relative lg:shadow-none w-full xl:max-w-[1920px] mx-auto xl:h-[60px] flex items-center justify-between`}>
-                <div className={`${isOpenMobileNav && 'h-screen'} xl:h-auto flex flex-col xl:flex-row gap-[50] items-center xl:ml-[40px] xl:mr-0 min-w-full xl:min-w-fit`}>
+            {/*    ${(mainPage && !cookieConsentIsTrue) && "mt-[40px]"} */}
+            <nav
+                className={`px-[1rem] bg-white shadow-[0_0_10px_rgba(0,0,0,0.15)] xl:h-[60px] w-full fixed top-0 z-50`}>
+                <div
+                    className={`relative lg:shadow-none w-full xl:max-w-[1920px] mx-auto xl:h-[60px] flex items-center justify-between`}>
                     <div
-                        className={`w-full flex justify-between ${!isOpenMobileNav ? "mb-[20px]" : "mb-[35px]"} mt-[20px] xl:mb-0 xl:mt-0`}>
-                        <Link href="/">
-                            <a><Logo/></a>
-                        </Link>
-                        <div className="xl:hidden">
-                            <Burger opened={isOpenMobileNav}
-                                    onClick={() => {
-                                        setIsOpenMobileNav((o) => !o);
-                                        setScrollLocked((c) => !c);
-                                    }}/>
-                        </div>
-                    </div>
-                    <div onClick={() => {
-                        setIsOpenMobileNav(false);
-                        setScrollLocked(() => false)
-                    }} className={`${!isOpenMobileNav && "hidden"} my-auto flex xl:flex flex-col xl:flex-row w-full gap-[25px] items-center xl:h-auto xl:min-w-full`}>
+                        className={`${isOpenMobileNav && 'h-screen'} xl:h-auto flex flex-col xl:flex-row gap-[50] items-center xl:ml-[40px] xl:mr-0 min-w-full xl:min-w-fit`}>
                         <div
-                            className={'flex flex-col xl:flex-row gap-[35px] xl:gap-[25px] text-[20px] xl:text-[16px] xl:mt-0'}>
-                            <div className={'xl:hidden flex flex-col gap-[35px]'}>
-                                <Link href={'/ponuka-bytov'} className={'xl:hidden'}>
-                                    <a>Ponuka apartmánov</a>
-                                </Link>
-                                <Link href={'/ponuka-bytov'} className={'xl:hidden'}>
-                                    <a>Garážové parkovanie</a>
-                                </Link>
+                            className={`w-full flex justify-between ${!isOpenMobileNav ? "mb-[20px]" : "mb-[35px]"} mt-[20px] xl:mb-0 xl:mt-0`}>
+                            <Link href="/">
+                                <a><Logo/></a>
+                            </Link>
+                            <div className="xl:hidden">
+                                <Burger opened={isOpenMobileNav}
+                                        onClick={() => {
+                                            setIsOpenMobileNav((o) => !o);
+                                            setScrollLocked((c) => !c);
+                                        }}/>
                             </div>
-                            <div className="hidden xl:block flex gap-[10px] items-center">
-                                <div className="dropdown">
-                                    <Link href="/ponuka-bytov">
-                                        <a className="drop-span">Ponuka apartmánov</a>
+                        </div>
+                        <div onClick={() => {
+                            setIsOpenMobileNav(false);
+                            setScrollLocked(() => false)
+                        }}
+                             className={`${!isOpenMobileNav && "hidden"} my-auto flex xl:flex flex-col xl:flex-row w-full gap-[25px] items-center xl:h-auto xl:min-w-full`}>
+                            <div
+                                className={'flex flex-col xl:flex-row gap-[35px] xl:gap-[25px] text-[20px] xl:text-[16px] xl:mt-0'}>
+                                <div className={'xl:hidden flex flex-col gap-[35px]'}>
+                                    <Link href={'/ponuka-bytov'} className={'xl:hidden'}>
+                                        <a>Ponuka apartmánov</a>
                                     </Link>
-                                    <div className="dropdown-content">
-                                        <Link href="/ponuka-bytov#garazove-parkovanie">
-                                            <a className="w-[180px] pt-[20px] pb-[20px] px-[10px] text-center">Garážové
-                                                parkovanie</a>
+                                    <Link href={'/ponuka-bytov#garazove-parkovanie'} className={'xl:hidden'}>
+                                        <a>Garážové parkovanie</a>
+                                    </Link>
+                                </div>
+                                <div className="hidden xl:block flex gap-[10px] items-center">
+                                    <div className="dropdown">
+                                        <Link href="/ponuka-bytov">
+                                            <a className="drop-span">Ponuka apartmánov</a>
                                         </Link>
-                                    </div>
-                                    <div className="ml-[10px] inline-block">
-                                        <DownArrowIcon fill={'black'}/>
+                                        <div className="dropdown-content">
+                                            <Link href="/ponuka-bytov#garazove-parkovanie">
+                                                <a className="w-[180px] pt-[20px] pb-[20px] px-[10px] text-center">Garážové
+                                                    parkovanie</a>
+                                            </Link>
+                                        </div>
+                                        <div className="ml-[10px] inline-block">
+                                            <DownArrowIcon fill={'black'}/>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <Link href="/lokalita">
-                                <a>Lokalita</a>
-                            </Link>
-                            <div className={'flex flex-col gap-[35px] xl:hidden'}>
-                                <Link href="/o-projekte">
-                                    <a>O projekte</a>
+                                <Link href="/lokalita">
+                                    <a>Lokalita</a>
                                 </Link>
-                                <Link href="/o-projekte#harmonogram">
-                                    <a>Harmonogram</a>
-                                </Link>
-                                <Link href="/o-projekte#financovanie">
-                                    <a>Financovanie</a>
-                                </Link>
-                            </div>
-                            <div className={'hidden xl:block'}>
-                                <div className="dropdown">
+                                <div className={'flex flex-col gap-[35px] xl:hidden'}>
                                     <Link href="/o-projekte">
                                         <a>O projekte</a>
                                     </Link>
-                                    <div className="dropdown-content">
-                                        <Link href="/o-projekte#harmonogram">
-                                            <a className="pt-[20px] pb-[10px] px-[20px]">Harmonogram</a>
+                                    <Link href="/o-projekte#harmonogram">
+                                        <a>Harmonogram</a>
+                                    </Link>
+                                    <Link href="/o-projekte#financovanie">
+                                        <a>Financovanie</a>
+                                    </Link>
+                                </div>
+                                <div className={'hidden xl:block'}>
+                                    <div className="dropdown">
+                                        <Link href="/o-projekte">
+                                            <a>O projekte</a>
                                         </Link>
-                                        <Link href="/o-projekte#financovanie">
-                                            <a className="pt-0 pb-[20px] px-[20px] text-[#476761]">Financovanie</a>
-                                        </Link>
-                                    </div>
-                                    <div className="ml-[10px] inline-block">
-                                        <DownArrowIcon/>
+                                        <div className="dropdown-content">
+                                            <Link href="/o-projekte#harmonogram">
+                                                <a className="pt-[20px] pb-[10px] px-[20px]">Harmonogram</a>
+                                            </Link>
+                                            <Link href="/o-projekte#financovanie">
+                                                <a className="pt-0 pb-[20px] px-[20px] text-[#476761]">Financovanie</a>
+                                            </Link>
+                                        </div>
+                                        <div className="ml-[10px] inline-block">
+                                            <DownArrowIcon/>
+                                        </div>
                                     </div>
                                 </div>
+                                <Link href="/retail">
+                                    <a>Retail</a>
+                                </Link>
+                                {/*<Link href="/novinky">*/}
+                                {/*    <a>Novinky</a>*/}
+                                {/*</Link>*/}
+                                <Link href="/kontakt">
+                                    <a>Kontakt</a>
+                                </Link>
                             </div>
-                            <Link href="/retail">
-                                <a>Retail</a>
-                            </Link>
-                            {/*<Link href="/novinky">*/}
-                            {/*    <a>Novinky</a>*/}
-                            {/*</Link>*/}
-                            <Link href="/kontakt">
-                                <a>Kontakt</a>
+                            <Link href={'/stretnutie'}>
+                                <button
+                                    className={'bg-[#476761] xl:hidden px-[10px] py-[6px] w-full text-white'}>Nezáväzné
+                                    stretnutie
+                                </button>
                             </Link>
                         </div>
-                        <button className={'bg-[#476761] xl:hidden px-[10px] py-[6px] w-full text-white'}>fff</button>
                     </div>
-                </div>
-                <div className="flex gap-[27px] mr-[40px]">
-                    <div className="hidden flex items-center gap-[5px]">
-                        <div className="languages">
-                            <Select
-                                className="hidden w-[50px]"
-                                data={languages}
-                                placeholder="SK"
-                                variant="unstyled"
-                                rightSection={<DownArrowIcon fill={'black'}/>}
-                                rightSectionWidth={30}
-                                styles={{rightSection: {pointerEvents: 'none'}}}
-                                transitionDuration={80}
-                            />
+                    <div className="flex gap-[27px] mr-[40px]">
+                        <div className="hidden flex items-center gap-[5px]">
+                            <div className="languages">
+                                <Select
+                                    className="hidden w-[50px]"
+                                    data={languages}
+                                    placeholder="SK"
+                                    variant="unstyled"
+                                    rightSection={<DownArrowIcon fill={'black'}/>}
+                                    rightSectionWidth={30}
+                                    styles={{rightSection: {pointerEvents: 'none'}}}
+                                    transitionDuration={80}
+                                />
+                            </div>
                         </div>
+                        <Link href='/stretnutie'>
+                            <button
+                                className={`hidden xl:block hover:bg-[#0E3F3B] font-medium text-[16px] leading-6 text-white bg-[#476761] px-[10px] py-[6px] xl:w-[190px]`}
+                                onClick={() => setIsOpenMobileNav(false)}>Nezáväzné
+                                stretnutie
+                            </button>
+                        </Link>
                     </div>
-                    <Link href='/stretnutie'>
-                        <button
-                            className={`hidden xl:block hover:bg-[#0E3F3B] font-medium text-[16px] leading-6 text-white bg-[#476761] px-[10px] py-[6px] xl:w-[190px]`} onClick={() => setIsOpenMobileNav(false)}>Nezáväzné
-                            stretnutie
-                        </button>
-                    </Link>
                 </div>
-            </div>
-        </nav>
+            </nav>
         </>
     );
 };
