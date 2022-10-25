8bdf238e4edf342d4a225ae54a343665f6ca2431
OliverSoucc <oliv572v@easv365.dk>
2022-10-25 18:47:06 +0200

    Tag Manager and Cookiebot

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,16 +6,16 @@ export default function Document() {
         <Html>
             <Head />
             <body>
-            <Main />
-            <NextScript />
-            <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="fefafcaa-5d1e-4369-84f5-b19f54db2b38" data-blockingmode="auto" type="text/javascript" strategy="beforeInteractive"/>
             {/*Google Tag Manager (noscript)*/}
             <noscript>
                 <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCZ89HB"
-                              height="0" width="0" style={{display:'none', visibility: 'hidden'}}>
+                        height="0" width="0" style={{display:'none', visibility: 'hidden'}}>
                 </iframe>
             </noscript>
             {/*End Google Tag Manager (noscript) */}
+            <Main />
+            <NextScript />
+            <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="fefafcaa-5d1e-4369-84f5-b19f54db2b38" data-blockingmode="auto" type="text/javascript" strategy="beforeInteractive"/>
             </body>
         </Html>
     )
