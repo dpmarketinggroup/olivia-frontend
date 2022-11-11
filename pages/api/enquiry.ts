cd53fbdc8e3975fa8537efb709515098dfa46c41
pelonidas <janx803y@easv365.dk>
2022-11-11 16:03:33 +0100

    ci: cookies acceptance integrated

--- pages/api/enquiry.ts
+++ pages/api/enquiry.ts
@@ -81,7 +81,7 @@ export default async function handler(
           ? "predaj@oliviaresidence.sk"
           : "info@oliviaresidence.sk"
       }`,
-
+      // dd
       html: `
               <div>
                   <h5><span style="text-decoration: underline">Meno:</span> ${name} ${surname}</h5>
