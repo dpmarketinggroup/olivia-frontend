e54774bb77c8c9d2c5f71f0505af613077ba1d6e
pelonidas <janx803y@easv365.dk>
2022-11-11 16:22:56 +0100

    ci: cookies acceptance integrated

--- pages/api/enquiry.ts
+++ pages/api/enquiry.ts
@@ -53,7 +53,7 @@ export default async function handler(
           "Content-Type": "application/json",
           accept: "application/json",
           "api-key":
-            "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",
+            "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",
         },
       }
     );
