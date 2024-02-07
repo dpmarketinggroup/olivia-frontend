dfd65101fa4c7765314d7cebb441ebf0fe53ac8f
Miňo <milkle005@gmail.com>
2024-02-07 13:14:29 +0100

    nova karta vybavenia druhy pokus

--- pages/o-projekte.tsx
+++ pages/o-projekte.tsx
@@ -166,7 +166,7 @@ const OProjekte = () => {
           title={translate("equipment-heading")}
           href="/standart.pdf"
         />
-        <Equipment title={translate("equipment-heading-4")} href="/karta_vybavenia.pdf" />
+        <Equipment title={translate("equipment-heading-4")} href="/karta_vybavenia1.pdf" />
       </div>
       <div className="flex justify-center" id="financovanie">
         <div className="w-full">
