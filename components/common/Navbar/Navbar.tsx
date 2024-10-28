9c4c49a09a11a735ceeed5f94122a0bb19b398bc
Miňo <milkle005@gmail.com>
2024-10-28 19:25:39 +0100

    add to navbar 4

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -276,8 +276,8 @@ const Navbar: FunctionComponent<NavbarProps> = ({ mainPage = false }) => {
                   </a>
                 </Link>*/}
                 <Link href="/stresne-apartmany">
-                  <a className="    flex justify-center items-center">
-                    <p className="   ">{translate("footer-link-top-apartments")}</p>
+                  <a className="font-bold text-[#ffffff] bg-[#89A6A2]  flex justify-center items-center">
+                    <p className=" border-[#89A6A2] p-[1px] border-[2px]">{translate("footer-link-top-apartments")}</p>
                   </a>
                 </Link>
                 <Link href="/lokalita">
